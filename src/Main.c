caa588fd2a6f95253682372c74685611c19e655f
Crimson17 <46162167+Crimson17@users.noreply.github.com>
2021-07-12 18:59:00 +0200

    added char output

--- src/Main.c
+++ src/Main.c
@@ -12,36 +12,31 @@ int main(void)
     // Get file name input
     printf("File name: ");
     fgets(dataFileName, maxFileNameLen - 1, stdin);
-    for (int i = 0; i < maxFileNameLen; i++)
-    {
-        if (dataFileName[i] == '\n')
-        {
+    for (int i = 0; i < maxFileNameLen; i++) {
+        if (dataFileName[i] == '\n') {
             dataFileName[i] = '\0';
             break;
         }
     }
 
     // Compare file names
-    if (!strcmp(storeFileName, dataFileName))
-    {
+    if (!strcmp(storeFileName, dataFileName)) {
         printf("The file name can't be \"%s\"\nPress ENTER to exit...", storeFileName);
         getchar();
         return -1;
     }
 
     // Open file
-    FILE *dataFile = fopen(dataFileName, "rb");
-    if (dataFile == NULL)
-    {
+    FILE* dataFile = fopen(dataFileName, "rb");
+    if (dataFile == NULL) {
         printf("File \"%s\" doesn't exist! \nPress ENTER to exit...", dataFileName);
         getchar();
         return -1;
     }
 
     // Open/Create "bytes.txt"
-    FILE *storeFile = fopen(storeFileName, "w");
-    if (storeFile == NULL)
-    {
+    FILE* storeFile = fopen(storeFileName, "w");
+    if (storeFile == NULL) {
         fclose(dataFile);
         return -1;
     }
@@ -57,9 +52,8 @@ int main(void)
     fprintf(storeFile, "Data length: %d\n", dataLen);
 
     // Allocate memory for the file data
-    unsigned char *data = (unsigned char *)calloc(dataLen, sizeof(unsigned char));
-    if (data == NULL)
-    {
+    unsigned char* data = (unsigned char*)calloc(dataLen, sizeof(unsigned char));
+    if (data == NULL) {
         fclose(dataFile);
         return -1;
     }
@@ -72,16 +66,25 @@ int main(void)
     fclose(dataFile);
 
     // Print COLLUMN info
-    fprintf(storeFile, "             00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F");
+    fprintf(storeFile, "             00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F     0 1 2 3 4 5 6 7 8 9 A B C D E F");
 
-    // Print ROW info and byte data
+    // Print ROW info, byte data and data as chars
     int rowCounter = 0;
-    for (int i = 0; i < dataLen; i++)
-    {
-        if (i % 16 == 0)
-        {
-            rowCounter++;
+    for (int i = 0; i < dataLen; i++) {
+        if (i % 16 == 0) {
+            if (i != 0) {
+                fprintf(storeFile, "   ");
+                for (int j = 16; j > 0; j--) {
+                    if (*(data + i - j) > 31 && *(data + i - j) < 127) {
+                        fprintf(storeFile, "%c ", *(data + i - j));
+                    }
+                    else {
+                        fprintf(storeFile, ". ");
+                    }
+                }
+            }
             fprintf(storeFile, "\n0x%08X: ", rowCounter);
+            rowCounter++;
         }
         fprintf(storeFile, "0x%02X ", *(data + i));
     }
